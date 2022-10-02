02b7b6e03a1e7235f995ed62c635b8eb2375f281
Sean Allen <angstyloop@gmail.com>
2022-10-01 21:15:45 -0700

    Wrote chmodRecursive.js

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ don't belong in the "js-utils" repo.
 Synchronous. Recursively find and remove all subdirectories of @root named
 @target. Implements BFS with a FIFO queue.
 
+## chmodRecursive(root, perms)
+
+Synchronous. Recursively find and chmod all children of @root to @perms, where
+@perms is a string containing three digits, e.g. '777', for the octal code.
+
+Implements BFS with a FIFO queue.
+
 # Keywords
 
 node.js nodejs fs find exec rm rf node\_modules recursive recurse remove all
---
+++ chmodRecursive.js
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+
+const { readdirSync, lstatSync, isDirectory, chmodSync } = require('fs');
+const { join, posix: { basename } } = require('path');
+
+/** List subdirectories of target directory @d. NOT recursive.
+ * @param dir - (string) Target directory.
+ * @return (string[]) Subdirectories of @d.
+ */
+function listDirs(dir) {
+    return readdirSync(dir)
+            .map(it => join(dir, it))
+            .filter(it => lstatSync(it).isDirectory());
+}
+
+/** List files (and subdirectories) in target directory @dir. NOT recursive.
+ * @param dir - (string) Full or relative path of target directory.
+ * @return (string[]) Files of @dir.
+ */
+function listChildren(dir) {
+    return readdirSync(dir).map(it => join(dir, it));
+}
+
+/** chmod all files (and subdirectories) in a directory @dir, giving them new
+ * permissions @perms. NOT recursive.
+ *
+ * @param dir - (string) Full or relative path of target directory.
+ * @perms - (string) Permissions for chmod, as a string (e.g., '644')
+ */
+function chmodChildren(dir, perms) {
+    listChildren(dir).map(it => chmodSync(it, perms));
+}
+
+/* Starting in the directory named @root, recursively find and chmod all
+ * all files and subdirectories. The directory @root is also chmod'd.
+ * Implements BFS with a FIFO queue.
+ *
+ * @root - Full or relative path of directory to start in.
+ * @perms - (string) Permissions for chmod, as a string (e.g., '644')
+ *
+ */
+/*module.exports = */function chmodRecursive(root, perms) {
+    let dir = root;
+    chmodSync(root, perms);
+    chmodChildren(root, perms);
+    const dirs = listDirs(dir);
+    while (dirs.length) {
+        dir = dirs.shift();
+        chmodChildren(dir, perms);
+        dirs.push(...listDirs(dir));
+    }
+}
+
+const EXIT_SUCCESS = 0;
+const EXIT_FAILURE = 1;
+
+/** Driver code.
+ */
+function main() {
+    // "node" and the name of the script are always the first two arguments.
+    // Your command line arguments start at index=2 (the third argument -
+    // totally not confusing, right?)
+    if (process.argv.length !== 4) {
+        console.log('USAGE: ./chmodRecursive <DIRECTORY> <PERMISSIONS>');
+        console.log('PERMISSIONS is a three character string, like 777.');
+        process.exit(EXIT_FAILURE);
+    }
+    chmodRecursive(...process.argv.slice(2));
+    process.exit(EXIT_SUCCESS);
+}
+
+main();
